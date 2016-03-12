74c49d75860a1d608044a35e118185620c0d996b
nathan <nathan@dorkbox.com>
2016-03-12 03:25:34 +0100

    code polish

--- Dorkbox-Util/src/org/bouncycastle/math/ec/ECAccessor.java
+++ Dorkbox-Util/src/org/bouncycastle/math/ec/ECAccessor.java
@@ -18,6 +18,5 @@ package org.bouncycastle.math.ec;
 public class ECAccessor {
     public static void setCoordSystem(ECCurve curve, int coordinateSystem) {
         curve.coord = coordinateSystem;
-
     }
 }
