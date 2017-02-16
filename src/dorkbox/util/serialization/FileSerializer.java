001e84b0732e53fb1c8f5c8baca50a31ce517f14
nathan <nathan@dorkbox.com>
2017-02-16 11:49:36 +0100

    Added File serialization

---
+++ src/dorkbox/util/serialization/FileSerializer.java
@@ -0,0 +1,26 @@
+package dorkbox.util.serialization;
+
+import java.io.File;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+/**
+ * Serialize the path of a file instead of the File object
+ */
+public
+class FileSerializer extends Serializer<File> {
+
+    @Override
+    public void write(Kryo kryo, Output output, File file) {
+        output.writeString(file.getPath());
+    }
+
+    @Override
+    public File read(Kryo kryo, Input input, Class<File> type) {
+        String path  = input.readString();
+        return new File(path);
+    }
+}
