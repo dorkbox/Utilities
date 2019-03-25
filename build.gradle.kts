f87392e4ca22fa04535d161dd78c4807ba6598cb
nathan <nathan@dorkbox.com>
2019-03-25 23:59:14 +0100

    Updated version from 1.0 to 1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ object Extras {
     // set for the project
     const val description = "Utilities for use within Java projects"
     const val group = "com.dorkbox"
-    const val version = "1.0"
+    const val version = "1.1"
 
     // set as project.ext
     const val name = "Utilities"
