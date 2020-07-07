e2e83a1c86982572d3c086864ccd66ada5b17651
nathan <nathan@dorkbox.com>
2020-07-07 22:42:11 +0200

    Updated version from 1.5.5 to 1.5.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ object Extras {
     // set for the project
     const val description = "Utilities for use within Java projects"
     const val group = "com.dorkbox"
-    const val version = "1.5.5"
+    const val version = "1.5.6"
 
     // set as project.ext
     const val name = "Utilities"
