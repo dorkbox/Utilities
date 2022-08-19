845d4cfddbb70d46148d93182ab757075f460883
Robinson <robinson@dorkbox.com>
2022-08-19 23:30:26 +0200

    updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ object Extras {
     // set for the project
     const val description = "Utilities for use within Java projects"
     const val group = "com.dorkbox"
-    const val version = "1.29"
+    const val version = "1.30"
 
     // set as project.ext
     const val name = "Utilities"
--- src/dorkbox/util/Desktop.java
+++ src/dorkbox/util/Desktop.java
@@ -42,7 +42,7 @@ class Desktop {
      */
     public static
     String getVersion() {
-        return "1.29";
+        return "1.30";
     }
 
     static {
