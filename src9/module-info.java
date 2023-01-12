e3cd11bd3b7c561d004ac9f30e3cd0dded90dee6
Robinson <robinson@dorkbox.com>
2023-01-12 02:14:52 +0100

    Updated module-info.java

--- src9/module-info.java
+++ src9/module-info.java
@@ -1,14 +1,5 @@
 module dorkbox.utilities {
     exports dorkbox.exit;
-    exports dorkbox.jna;
-    exports dorkbox.jna.rendering;
-    exports dorkbox.jna.linux;
-    exports dorkbox.jna.linux.structs;
-    exports dorkbox.jna.macos;
-    exports dorkbox.jna.macos.cocoa;
-    exports dorkbox.jna.macos.foundation;
-    exports dorkbox.jna.windows;
-    exports dorkbox.jna.windows.structs;
     exports dorkbox.urlHandler;
     exports dorkbox.util;
     exports dorkbox.util.classes;
@@ -21,6 +12,7 @@ module dorkbox.utilities {
     exports dorkbox.util.userManagement;
 
     requires transitive dorkbox.executor;
+    requires transitive dorkbox.jna;
     requires transitive dorkbox.updates;
     requires transitive dorkbox.os;
 
@@ -31,5 +23,4 @@ module dorkbox.utilities {
     requires static com.sun.jna.platform;
 
     requires static org.slf4j;
-    requires static java.desktop;
 }
