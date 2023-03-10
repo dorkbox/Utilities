a044df099992eba644d1202b60c6de2e1a8afb4e
Robinson <robinson@dorkbox.com>
2023-03-09 18:15:40 -0600

    Removed async utility function. It's a delicate API, and should not be "made easy". Updated copyright.

--- src/dorkbox/util/CommonUtils.kt
+++ src/dorkbox/util/CommonUtils.kt
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2023 dorkbox, llc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * Copyright 2017 Pronghorn Technology LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,12 +32,6 @@
 
 package dorkbox.util
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
@@ -55,20 +65,6 @@ inline fun ignoreExceptions(vararg blocks: () -> Unit) {
         }
     }
 }
-
-fun async(dispatcher: CoroutineDispatcher, action: suspend CoroutineScope.() -> Unit): Job {
-    return GlobalScope.launch(dispatcher) {
-        action()
-    }
-}
-
-
-fun asyncIO(action: suspend CoroutineScope.() -> Unit): Job {
-    return GlobalScope.launch(Dispatchers.IO) {
-        action()
-    }
-}
-
 
 // From: https://elizarov.medium.com/phantom-of-the-coroutine-afc63b03a131
 suspend inline fun <T> Mutex.withReentrantLock(crossinline block: suspend () -> T): T {
