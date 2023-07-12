be2dfae82401159b834b5f57151d54fd1f34efba
Robinson <robinson@dorkbox.com>
2023-07-12 17:21:29 +0200

    Updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ object Extras {
     // set for the project
     const val description = "Utilities for use within Java projects"
     const val group = "com.dorkbox"
-    const val version = "1.42"
+    const val version = "1.43"
 
     // set as project.ext
     const val name = "Utilities"
