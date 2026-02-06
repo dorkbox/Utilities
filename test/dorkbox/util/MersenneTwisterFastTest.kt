c86445995842da8752372ca82274bb48daae2706
Robinson <robinson@dorkbox.com>
2026-02-06 13:22:18 +0100

    Fixed tests

--- test/dorkbox/util/MersenneTwisterFastTest.kt
+++ test/dorkbox/util/MersenneTwisterFastTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 dorkbox, llc
+ * Copyright 2026 dorkbox, llc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  */
 package dorkbox.util
 
+import org.junit.Assert
 import org.junit.Test
 import java.io.IOException
 import java.util.*
@@ -98,7 +99,7 @@ class MersenneTwisterFastTest {
             test = true
         }
         println(
-            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + "" + test
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
         )
         println("\nGrab 1000 booleans of increasing probability using nextBoolean(double)")
         r = MersenneTwisterFast(SEED)
@@ -119,7 +120,7 @@ class MersenneTwisterFastTest {
             test = true
         }
         println(
-            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + "" + test
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
         )
         println("\nGrab 1000 booleans of increasing probability using nextBoolean(float)")
         r = MersenneTwisterFast(SEED)
@@ -131,7 +132,7 @@ class MersenneTwisterFastTest {
             test = r.nextBoolean(j / 999.0f)
             if (j % 8 == 7) {
                 test = false
-                println()
+                // println()
             }
             j++
         }
@@ -140,8 +141,9 @@ class MersenneTwisterFastTest {
             test = true
         }
         println(
-            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + "" + test
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
         )
+
         val bytes = ByteArray(1000)
         println("\nGrab the first 1000 bytes using nextBytes")
         r = MersenneTwisterFast(SEED)
@@ -150,56 +152,82 @@ class MersenneTwisterFastTest {
         while (j < 1000) {
             print(bytes[j].toString() + " ")
             if (j % 16 == 15) {
-                println()
+                // println()
+                test = false
             }
             j++
         }
         if (j % 16 != 15) {
-            println()
+            // println()
+            test = true
         }
-        var b: Byte
+        println(
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
+        )
+
+
+        var b: Byte = 0
         println("\nGrab the first 1000 bytes -- must be same as nextBytes")
         r = MersenneTwisterFast(SEED)
         j = 0
         while (j < 1000) {
             print(r.nextByte().also { b = it }.toString() + " ")
             if (b != bytes[j]) {
-                print("BAD ")
+//                print("BAD ")
+                Assert.fail("Bytes not equal!")
             }
             if (j % 16 == 15) {
-                println()
+                test = true
+//                println()
             }
             j++
         }
         if (j % 16 != 15) {
-            println()
+//            println()
+            test = true
         }
+        println(
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
+        )
+
         println("\nGrab the first 1000 shorts")
         r = MersenneTwisterFast(SEED)
         j = 0
         while (j < 1000) {
             print(r.nextShort().toString() + " ")
             if (j % 8 == 7) {
-                println()
+//                println()
+                test = true
             }
             j++
         }
         if (j % 8 != 7) {
-            println()
+//            println()
+            test = false
         }
+        println(
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
+        )
+
         println("\nGrab the first 1000 ints")
         r = MersenneTwisterFast(SEED)
         j = 0
         while (j < 1000) {
             print(r.nextInt().toString() + " ")
             if (j % 4 == 3) {
-                println()
+//                println()
+                test = true
             }
             j++
         }
         if (j % 4 != 3) {
-            println()
+//            println()
+            test = false
         }
+        println(
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
+        )
+
         println("\nGrab the first 1000 ints of different sizes")
         r = MersenneTwisterFast(SEED)
         var max = 1
@@ -211,26 +239,39 @@ class MersenneTwisterFastTest {
                 max = 1
             }
             if (j % 4 == 3) {
-                println()
+//                println()
+                test = true
             }
             j++
         }
         if (j % 4 != 3) {
-            println()
+//            println()
+            test = false
         }
+        println(
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
+        )
+
         println("\nGrab the first 1000 longs")
         r = MersenneTwisterFast(SEED)
         j = 0
         while (j < 1000) {
             print(r.nextLong().toString() + " ")
             if (j % 3 == 2) {
-                println()
+//                println()
+                test = true
             }
             j++
         }
         if (j % 3 != 2) {
-            println()
+//            println()
+            test = false
         }
+        println(
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
+        )
+
+
         println("\nGrab the first 1000 longs of different sizes")
         r = MersenneTwisterFast(SEED)
         var max2: Long = 1
@@ -242,51 +283,77 @@ class MersenneTwisterFastTest {
                 max2 = 1
             }
             if (j % 4 == 3) {
-                println()
+//                println()
+                test = true
             }
             j++
         }
         if (j % 4 != 3) {
-            println()
+//            println()
+            test = false
         }
+        println(
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
+        )
+
+
         println("\nGrab the first 1000 floats")
         r = MersenneTwisterFast(SEED)
         j = 0
         while (j < 1000) {
             print(r.nextFloat().toString() + " ")
             if (j % 4 == 3) {
-                println()
+//                println()
+                test = true
             }
             j++
         }
         if (j % 4 != 3) {
-            println()
+//          println()
+            test = false
         }
+        println(
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
+        )
+
+
         println("\nGrab the first 1000 doubles")
         r = MersenneTwisterFast(SEED)
         j = 0
         while (j < 1000) {
             print(r.nextDouble().toString() + " ")
             if (j % 3 == 2) {
-                println()
+//                println()
+                test = true
             }
             j++
         }
         if (j % 3 != 2) {
-            println()
+//            println()
+            test = false
         }
+        println(
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
+        )
+
+
         println("\nGrab the first 1000 gaussian doubles")
         r = MersenneTwisterFast(SEED)
         j = 0
         while (j < 1000) {
             print(r.nextGaussian().toString() + " ")
             if (j % 3 == 2) {
-                println()
+//                println()
+                test = true
             }
             j++
         }
         if (j % 3 != 2) {
-            println()
+//            println()
+            test = false
         }
+        println(
+            "Mersenne Twister Fast: " + (System.currentTimeMillis() - ms) + "          Ignore this: " + xx + " " + test
+        )
     }
 }
--- test/dorkbox/util/TimeTest.kt
+++ test/dorkbox/util/TimeTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 dorkbox, llc
+ * Copyright 2026 dorkbox, llc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,41 +23,41 @@ class TimeTest {
     @Test
     fun time() {
         TimeUnit.DAYS.toNanos(3).also {
-            Assert.assertEquals("3 days", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("3 days", it.asTimeFull())
         }
         TimeUnit.DAYS.toNanos(30).also {
-            Assert.assertEquals("30 days", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("30 days", it.asTimeFull())
         }
         TimeUnit.DAYS.toNanos(300).also {
-            Assert.assertEquals("300 days", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("300 days", it.asTimeFull())
         }
         TimeUnit.DAYS.toNanos(3000).also {
-            Assert.assertEquals("3000 days", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("3000 days", it.asTimeFull())
         }
 
 
         TimeUnit.HOURS.toNanos(3).also {
-            Assert.assertEquals("3 hours", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("3 hours", it.asTimeFull())
         }
         TimeUnit.MINUTES.toNanos(3).also {
-            Assert.assertEquals("3 minutes", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("3 minutes", it.asTimeFull())
         }
         TimeUnit.SECONDS.toNanos(3).also {
-            Assert.assertEquals("3 seconds", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("3 seconds", it.asTimeFull())
         }
         TimeUnit.MILLISECONDS.toNanos(3).also {
-            Assert.assertEquals("3 milli-seconds", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("3 milli-seconds", it.asTimeFull())
         }
         TimeUnit.MICROSECONDS.toNanos(3).also {
-            Assert.assertEquals("3 micro-seconds", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("3 micro-seconds", it.asTimeFull())
         }
         TimeUnit.NANOSECONDS.toNanos(3).also {
-            Assert.assertEquals("3 nano-seconds", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("3 nano-seconds", it.asTimeFull())
         }
 
 
         TimeUnit.NANOSECONDS.toNanos(1).also {
-            Assert.assertEquals("1 nano-second", Sys.getTimePrettyFull(it))
+            Assert.assertEquals("1 nano-second", it.asTimeFull())
         }
     }
 }
